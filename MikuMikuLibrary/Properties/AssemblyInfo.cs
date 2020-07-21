793987224aeec9e3c062409ee2544f017eb8f7c6
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2020-07-21 14:42:04 +0300

    Fix typo from previous commit.
    Why did that compile?

--- MikuMikuLibrary/Properties/AssemblyInfo.cs
+++ MikuMikuLibrary/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 #if DEBUG
 [assembly: AssemblyVersion( "2.0.*" )]
 #else
-[assembly: AssemblyVersion( "2.0.1" )]
+[assembly: AssemblyVersion( "2.0.2" )]
 #endif
