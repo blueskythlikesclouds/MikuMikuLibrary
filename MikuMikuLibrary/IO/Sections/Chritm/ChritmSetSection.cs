26a645612516f1ceb33e5fca4327ec91fde56ec3
thatrandomlurker-divamoddingtools <59970354+thatrandomlurker-divamoddingtools@users.noreply.github.com>
2022-04-22 17:14:51 -0230

    Add Chritm Section Code

---
+++ MikuMikuLibrary/IO/Sections/Chritm/ChritmSetSection.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using MikuMikuLibrary.Chritm;
+using MikuMikuLibrary.IO.Sections.IO;
+
+namespace MikuMikuLibrary.IO.Sections.Chritm
+{
+    [Section("ITEM")]
+    public class AetSetSection : BinaryFileSection<ChritmSet>
+    {
+        public override SectionFlags Flags => SectionFlags.None;
+
+        public override Encoding Encoding { get; } = Encoding.GetEncoding("utf-8");
+
+        public AetSetSection(SectionMode mode, ChritmSet data = null) : base(mode, data)
+        {
+        }
+    }
+}
